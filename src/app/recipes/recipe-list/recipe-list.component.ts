7f1d7f26215ed53dc3a06204b0abb1bd9c709ab6
Emmanuel Koduah Tweneboah <tkayy729>
2023-05-09 07:29:20 +0000

    added two models and displayed them in the recipe list and shopping list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] =[
+    new Recipe("Cake", "Delicious cake made by myself" ,`https://preppykitchen.com/wp-content/uploads/2022/05/Strawberry-Cake-Recipe-Card.jpg`),
+    new Recipe("Jollof", "Ghana Jollof with pear", "https://eatwellabi.com/wp-content/uploads/2022/11/Jollof-rice-16.jpg")
+  ]
+
+  
+    
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients =[
+    new Ingredient("mako", 89),
+    new Ingredient("tomato",78),
+    new Ingredient("shio",797),
+    new Ingredient("pepper",178)
+  ]
 
 }
